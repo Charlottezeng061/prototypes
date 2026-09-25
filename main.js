a126bf9c68909239a41b3fb26842e7197bbd555b
Charlotte061 <s4160768@student.rmit.edu.au>
2026-09-25 20:29:01 +1000

    fix audio problems

--- main.js
+++ main.js
@@ -9,64 +9,63 @@
 
 // Genre sounds
 let fantasySounds = [
-    "../assets/audio/genre/fantasy1.wav",
-    "../assets/audio/genre/fantasy2.wav",
-    "../assets/audio/genre/fantasy3.wav",
-    "../assets/audio/genre/fantasy4.wav"
+    "./assets/audio/genre/fantasy1.wav",
+    "./assets/audio/genre/fantasy2.wav",
+    "./assets/audio/genre/fantasy3.wav",
+    "./assets/audio/genre/fantasy4.wav"
 ];
 
 let mysterySounds = [
-    "../assets/audio/genre/mystery1.wav",
-    "../assets/audio/genre/mystery2.wav",
-    "../assets/audio/genre/mystery3.wav",
-    "../assets/audio/genre/mystery4.wav"
+    "./assets/audio/genre/mystery1.wav",
+    "./assets/audio/genre/mystery2.wav",
+    "./assets/audio/genre/mystery3.wav",
+    "./assets/audio/genre/mystery4.wav"
 ];
 
 let romanceSounds = [
-    "../assets/audio/genre/romance1.wav",
-    "../assets/audio/genre/romance2.wav",
-    "../assets/audio/genre/romance3.wav",
-    "../assets/audio/genre/romance4.wav"
+    "./assets/audio/genre/romance1.wav",
+    "./assets/audio/genre/romance2.wav",
+    "./assets/audio/genre/romance3.wav",
+    "./assets/audio/genre/romance4.wav"
 ];
 
 let sciFiSounds = [
-    "../assets/audio/genre/sci-fi1.wav",
-    "../assets/audio/genre/sci-fi2.wav",
-    "../assets/audio/genre/sci-fi3.wav",
-    "../assets/audio/genre/sci-fi4.wav"
+    "./assets/audio/genre/sci-fi1.wav",
+    "./assets/audio/genre/sci-fi2.wav",
+    "./assets/audio/genre/sci-fi3.wav",
+    "./assets/audio/genre/sci-fi4.wav"
 ];
 
 
 // Atmosphere sounds
 
 let calmSounds = [
-    "../assets/audio/atmosphere/calm1.wav",
-    "../assets/audio/atmosphere/calm2.wav",
-    "../assets/audio/atmosphere/calm3.wav",
-    "../assets/audio/atmosphere/calm4.wav"
+    "./assets/audio/atmosphere/calm1.wav",
+    "./assets/audio/atmosphere/calm2.wav",
+    "./assets/audio/atmosphere/calm3.wav",
+    "./assets/audio/atmosphere/calm4.wav"
 ];
 
 let darkSounds = [
-    "../assets/audio/atmosphere/dark1.wav",
-    "../assets/audio/atmosphere/dark2.wav",
-    "../assets/audio/atmosphere/dark3.wav",
-    "../assets/audio/atmosphere/dark4.wav"
+    "./assets/audio/atmosphere/dark1.wav",
+    "./assets/audio/atmosphere/dark2.wav",
+    "./assets/audio/atmosphere/dark3.wav",
+    "./assets/audio/atmosphere/dark4.wav"
 ];
 
 let magicalSounds = [
-    "../assets/audio/atmosphere/magical1.wav",
-    "../assets/audio/atmosphere/magical2.wav",
-    "../assets/audio/atmosphere/magical3.wav",
-    "../assets/audio/atmosphere/magical4.wav"
+    "./assets/audio/atmosphere/magical1.wav",
+    "./assets/audio/atmosphere/magical2.wav",
+    "./assets/audio/atmosphere/magical3.wav",
+    "./assets/audio/atmosphere/magical4.wav"
 ];
 
 let nostalgicSounds = [
-    "../assets/audio/atmosphere/nostalgic1.wav",
-    "../assets/audio/atmosphere/nostalgic2.wav",
-    "../assets/audio/atmosphere/nostalgic3.wav",
-    "../assets/audio/atmosphere/nostalgic4.wav"
+    "./assets/audio/atmosphere/nostalgic1.wav",
+    "./assets/audio/atmosphere/nostalgic2.wav",
+    "./assets/audio/atmosphere/nostalgic3.wav",
+    "./assets/audio/atmosphere/nostalgic4.wav"
 ];
-
 
 
 ///////////// Audio Names
@@ -147,7 +146,6 @@ const atmosphereSelect =
     document.getElementById("atmosphere-select");
 
 
-
 ///////////// Lock Checkboxes
 
 // checkbox values are checked using .checked
@@ -161,7 +159,6 @@ const atmosphereLock =
     document.getElementById("atmosphere-lock");
 
 
-
 ///////////// Generated Results
 
 const genreResult =
@@ -169,7 +166,6 @@ const genreResult =
 
 const atmosphereResult =
     document.getElementById("atmosphere-result");
-
 
 
 ///////////// Audio
@@ -186,12 +182,10 @@ const atmosphereAudio =
     document.getElementById("atmosphere-audio");
 
 
-
 // set a comfortable default volume
 
 genreAudio.volume = 0.5;
 atmosphereAudio.volume = 0.5;
-
 
 
 /////////// Buttons
